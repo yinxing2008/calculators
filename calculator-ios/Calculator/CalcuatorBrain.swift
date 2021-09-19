d1233db98ee5245a2cdc3450dad8be281669fc19
jerry <309032663@qq.com>
2021-09-19 22:09:34 +0800

    update

--- calculator-ios/Calculator/CalcuatorBrain.swift
+++ calculator-ios/Calculator/CalcuatorBrain.swift
@@ -1,10 +1,5 @@
-//
-//  CalcuatorBrain.swift
-//  Calculator
-//
-//  Created by weiwu du on 2017/12/13.
-//  Copyright © 2017年 suning. All rights reserved.
-//
+// 花生皮编程(CSDN、简书、掘金、今日头条、微信公众号、抖音、快手、B站、西瓜视频)
+// 编程学习资料及开源项目见：https://juejin.cn/post/7002792005688360968
 
 import Foundation
 
